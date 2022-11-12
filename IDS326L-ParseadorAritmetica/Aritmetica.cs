6fe4be015009abaa75892fca987be4ed12b65bb1
Vladimir González <73143660+mrvladimir15@users.noreply.github.com>
2022-11-12 17:46:59 -0400

    Add project files.

---
+++ IDS326L-ParseadorAritmetica/Aritmetica.cs
@@ -0,0 +1,36 @@
+﻿using Antlr4.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDS326L_ParseadorAritmetica
+{
+    internal class Aritmetica : aritmeticaBaseVisitor<int>
+    {
+        public override int VisitAritmetica([NotNull] aritmeticaParser.AritmeticaContext context)
+        {
+            Console.WriteLine($"Visitando aritmética: {context.GetText()}");
+            return base.VisitAritmetica(context);
+        }
+
+        public override int VisitExpresion([NotNull] aritmeticaParser.ExpresionContext context)
+        {
+            Console.WriteLine($"Visitando expresión: {context.GetText()}");
+            return base.VisitExpresion(context);
+        }
+
+        public override int VisitFactor([NotNull] aritmeticaParser.FactorContext context)
+        {
+            Console.WriteLine($"Visitando factor: {context.GetText()}");
+            return base.VisitFactor(context);
+        }
+
+        public override int VisitTermino([NotNull] aritmeticaParser.TerminoContext context)
+        {
+            Console.WriteLine($"Visitando término: {context.GetText()}");
+            return base.VisitTermino(context);
+        }
+    }
+}
---
+++ IDS326L-ParseadorAritmetica/Program.cs
@@ -0,0 +1,12 @@
+﻿using Antlr4.Runtime;
+using IDS326L_ParseadorAritmetica;
+
+string input = "5*6+9*3-1";
+Console.WriteLine($"Input: {input}");
+ICharStream stream = CharStreams.fromString(input);
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+aritmeticaParser parser = new aritmeticaParser(tokens);
+var tree = parser.aritmetica();
+Aritmetica aritmetica = new Aritmetica();
+int res = aritmetica.Visit(tree);
